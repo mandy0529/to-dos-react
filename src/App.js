ae77d92c7ff65f83b81945505114c4ffe927cc04
Minji <321963741@naver.com>
2021-08-21 12:20:46 +0900

    failed today

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 // import TranslateProvider from './TransContext';
 // import translation from './Translation';
 // import TransScreen from './TransScreen';
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 
 const ADD = 'add';
-const MINUS = 'Minus';
 
-const reducer = (state, action) => {
-  switch (action) {
+const initialState = {
+  toDos: [],
+};
+
+const addToDo = (state, action) => {
+  switch (action.type) {
     case ADD:
-      return {count: state.count + 1};
+      return {
+        toDos: [[...state.toDos], {text: action.payload}],
+      };
     default:
       return;
   }
 };
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, {count: 0});
+  const [state, dispatch] = useReducer(addToDo, initialState);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({type: ADD, payload: newTodo});
+  };
+  const handleChange = (event) => {
+    const {value} = event.target;
+    console.log(value);
+    setNewTodo(value);
+  };
+
   return (
     <>
-      <h1>to dos</h1>
+      <h1>add to do</h1>
       <div>
-        <form>
-          <input placeholder="write a to do"></input>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            value={newTodo}
+            placeholder="write a to do"
+          ></input>
+          <button onClick={() => dispatch(ADD)}>+</button>
         </form>
-        <button onClick={() => dispatch(ADD)}>+</button>
       </div>
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map((toDo, index) => (
+          <li key={index}>{toDo.text}</li>
+        ))}
+      </ul>
     </>
     // <TranslateProvider defaultLang="en" translation={translation}>
     //   <TransScreen />
